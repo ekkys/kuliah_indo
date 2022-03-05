28aa5393ffee9dd20ea0fe81b86d1f603735c298
Chalid Ade Rahman <chalid.aderahman@kpiinstitute.org>
2022-03-05 11:25:27 +0700

    update assets path

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -171,7 +171,7 @@
 
   <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
   
-  <script src="{{ asset('asstes/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   
   <script src="{{ asset('assets/dist/js/adminlte.min.js') }}"></script>
 </body>
