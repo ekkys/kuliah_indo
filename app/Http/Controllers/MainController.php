46a39c2caea78dcf6e5384d516e7468ce225b7b0
ekkys99 <ekkys99@gmail.com>
2022-03-26 10:07:54 +0700

    [ = ] Update dinamis slide banner

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UploadSlideBanner;
 
 class MainController extends Controller
 {
     public function index() {
-        return view('mainWeb.home');
+        return view('mainWeb.home', [
+            'slidebanners' => UploadSlideBanner::orderBy('updated_at', 'DESC')->get()
+        ]);
     }
 }
