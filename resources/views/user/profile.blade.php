b76a4323b76e97a5c6400bb0702d8e8119a822e1
Chalid Ade Rahman <chalid.aderahman@kpiinstitute.org>
2022-04-09 15:56:01 +0700

    update

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -18,19 +18,19 @@
                                    </div>
                                    <div class="form-group">
                                           <label for="form-full-name">Full Name</label>
-                                          <input type="text" class="form-control" id="form-full-name" aria-describedby="emailHelp" placeholder="Full Name" value="Cevin Leonardo">
+                                          <input type="text" class="form-control" id="form-full-name" aria-describedby="emailHelp" placeholder="Full Name" value={{$user->name}}>
                                    </div>
                                    <div class="form-group">
                                           <label for="form-email">Email</label>
-                                          <input type="email" class="form-control-plaintext" id="form-email" placeholder="Email" value="test@test" readonly>
+                                          <input type="email" class="form-control-plaintext" id="form-email" placeholder="Email" value={{$user->email}} readonly>
                                    </div>
                                    <div class="form-group">
                                           <label for="form-phone">Phone</label>
-                                          <input type="text" class="form-control" id="form-phone" placeholder="Phone" value="081234567890">
+                                          <input type="text" class="form-control" id="form-phone" placeholder="Phone" value={{isset($user->phone) ? $user->phone : "" }}>
                                    </div>
                                    <div class="form-biography mb-4">
                                           <label for="form-biography">Biography</label>
-                                          <textarea class="form-control" id="form-biography" rows="3" >Saya merasa paling ganteng diantara pria lain</textarea>
+                                          <textarea class="form-control" id="form-biography" rows="3" >{{isset($user->biography) ? $user->phone : "" }}</textarea>
                                    </div>
                                    <button type="submit" class="btn btn-confirm-form">Submit</button>
                             </form>
