5bc5f43919c5eb5364bf73219306e60082ce1891
ekkys99 <ekkys99@gmail.com>
2022-04-15 16:24:22 +0700

    [ - ] Buy course button masih error

--- resources/views/mainWeb/singleClass.blade.php
+++ resources/views/mainWeb/singleClass.blade.php
@@ -77,9 +77,118 @@
                         </div>
                         <div class="col-4 text-center">
                             <div class="cart-wrapper">
-                                <button class="add-to-cart-button">Buy Course</button>
+                                <button class="add-to-cart-button"  data-toggle="modal" data-target="#modalBuy" >Buy Course</button>
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                   Buy Course
+                                  </button>
                             </div>
                         </div>
+
+                        <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+                         {{-- modal --}}
+                         <div class="modal fade bd-example-modal-lg" id="modalBuy" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered modal-lg">
+                              <div class="modal-content">
+                                <div class="modal-header justify-content-center">
+                                  <h5 class="modal-title font-weight-bold" id="exampleModalLongTitle">Payment Detail</h5>
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                  </button>
+                                </div>
+                                <div class="modal-body overflow-auto p-0">
+                                  {{-- content modal --}}
+
+                                   <div class="container-trans text-align-left">
+                                     <div class="body-trans">
+                                       <div class="main-status">
+                                         <p class="main-text font-weight-bold text-dark">Order Number</p>
+                                         <div>
+                                           <p class="main-text-desc">#1003528254</p>
+                                         </div>
+                                       </div>
+                                     </div>
+                                     <div class="body-trans">
+                                       <div class="main-status">
+                                         <p class="main-text font-weight-bold">Invoice Number</p>
+                                         <div>
+                                           <a href="{{ url('/invoice') }}" target="__blank" class="main-text-desc">INV/20220308/COU/1003528254</a>
+                                         </div>
+                                       </div>
+                                     </div>
+                                     <div class="body-trans">
+                                       <div class="main-status">
+                                         <p class="main-text font-weight-bold">Status Detail</p>
+                                       </div>
+                                       <div class="main-status mt-2">
+                                         <p class="main-text">Purchase Complete</p>
+                                         <div>
+                                           <p href="#" target="__blank" class="main-text">Aug 12, 2022, 07:15 WIB</p>
+                                         </div>
+                                       </div>
+                                       <div class="main-status mt-2">
+                                         <p class="main-text">Purchase Verification</p>
+                                         <div>
+                                           <p href="#" target="__blank" class="main-text">Aug 12, 2022, 07:10 WIB</p>
+                                         </div>
+                                       </div>
+                                       <div class="main-status mt-2">
+                                         <p class="main-text">Purchase Date</p>
+                                         <div>
+                                           <p href="#" target="__blank" class="main-text">Aug 12, 2022, 07:06 WIB</p>
+                                         </div>
+                                       </div>
+                                     </div>
+                                   </div>
+
+                                   <div class="container-trans text-align-left">
+                                     <div class="body-trans">
+                                       <div class="main-status mb-2">
+                                         <p class="main-text font-weight-bold text-dark">Detail Product</p>
+                                       </div>
+                                       <section class="product-card">
+                                         <div class="product-section">
+                                           <div class="wrapper-product">
+                                             <div class="product-info">
+                                               <img src="{{ asset('mainWeb/images/class/class1.webp') }}">
+                                               <div>
+                                                 <a href="#" class="main-text font-weight-bold text-dark">Fundamental Pemrograman CNC, CAD/CAM Dan Simulator CNC</a>
+                                               </div>
+                                             </div>
+                                           </div>
+                                           <div class="wrapper-price">
+                                             <div class="total-price">
+                                               <p class="price-heading">Total Price</p>
+                                               <h5 class="main-price">Rp 499.000</h5>
+                                             </div>
+                                           </div>
+                                         </div>
+                                       </section>
+                                     </div>
+                                   </div>
+
+                                  {{-- end content modal --}}
+                                </div>
+                              </div>
+                            </div>
+                        </div>
+                        {{-- end modal --}}
                     </div>
                 </div>
             </div>
@@ -92,6 +201,7 @@
             {{-- End Description --}}
 
         </div>
+                                   
     </section>
     <!-- End Single Post Article -->
 
